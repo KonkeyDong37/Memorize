fd10ce8bdb11d6d9bb7d91a7785d183aa0daf5aa
Andrey <KonkeyDong37@gmail.com>
2021-06-05 13:03:32 +0500

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Андрей on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
