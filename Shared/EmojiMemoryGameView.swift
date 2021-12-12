8d51a68821761216dceda6a803433ae8f42b6eb4
Andrey <KonkeyDong37@gmail.com>
2021-12-12 14:43:09 +0500

    Add animation and bonus system

--- Shared/EmojiMemoryGameView.swift
+++ Shared/EmojiMemoryGameView.swift
@@ -42,9 +42,23 @@ struct EmojiMemoryGameView: View {
                             CardView(card: card)
                                 .padding(5)
                                 .onTapGesture {
-                                    viewModel.choose(card: card)
+                                    withAnimation(.easeInOut) {
+                                        viewModel.choose(card: card)
+                                    }
                                 }
                         }
+                    }
+                    Divider()
+                    HStack {
+                        Spacer()
+                        Button {
+                            withAnimation(.linear) {
+                                viewModel.resetGame()
+                            }
+                        } label: {
+                            Text("Reset game")
+                        }
+
                     }
                 }
             }
@@ -57,18 +71,35 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     
     var card: MemoryGame<String>.Card
+    @State private var animatedBonusRemaining: Double = 0
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
     
     var body: some View {
         GeometryReader { geometry in
             if card.isFaceUp || !card.isMatched {
                 ZStack {
-                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                        .padding(6)
-                        .opacity(0.6)
+                    Group {
+                        if card.isConsumingBonusTime {
+                            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                                .onAppear {
+                                    startBonusTimeAnimation()
+                                }
+                        } else {
+                            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                        }
+                    }
+                    .padding(6)
+                    .opacity(0.6)
                     Text(card.content)
                         .font(Font.system(size: fontSize(for: geometry.size)))
                 }
                 .cardify(isFaceUp: card.isFaceUp)
+                .transition(.scale)
             }
         }
     }
--- Shared/MemoryGame.swift
+++ Shared/MemoryGame.swift
@@ -56,10 +56,80 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
+        var isFaceUp: Bool = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        var isMatched: Bool = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
         var content: CardContent
         var id: Int
         var alreadyViewed: Bool = false
+        
+        //MARK: - Bonus Time
+        
+        // this could give matching bonus points
+        // if the user matches card before a certian amount
+        // of time asses durring with e card is face up
+        
+        // can be zero which means "no bonus available" for this card
+        var bonusTimeLimit: TimeInterval = 6
+        
+        // how long this card has ever been face up
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        // the last time this card was turned face up (and is steel is face up)
+        var lastFaceUpDate: Date?
+        
+        // the accumulated time this card has been face up in the past
+        // (i.e. not including the current time it's been face up if it is currently so)
+        var pastFaceUpTime: TimeInterval = 0
+        
+        // how match time left before the bonus opportunity runs out
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        
+        // percentage of the bonus time remaining
+        var bonusRemaining: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining / bonusTimeLimit : 0
+        }
+        
+        // whether the card was mathed during the bonus time period
+        var hasEarnedBonus: Bool {
+            isMatched && bonusRemaining > 0
+        }
+        
+        // wether we are currently face up, unmatched and have not yet used up the bonsu window
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        // called when the card transitions to face up state
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        
+        // called when the card goes back face down (or gets matched)
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUpTime
+            self.lastFaceUpDate = nil
+        }
     }
 }
